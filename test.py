f57c122fa99445ee34c35120cacb47821b755378
shaheermalayil <shaheermalayil@users.noreply.github.com>
2018-03-27 12:01:17 +0530

    added new files

--- test.py
+++ test.py
@@ -1,26 +1,17 @@
-from database import QUERIES
-from flask import Flask,render_template,redirect,url_for,session,request
-import os
-from werkzeug.security import generate_password_hash,check_password_hash
-from flask_bootstrap import Bootstrap
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
-
-
-app = Flask(__name__)
-Bootstrap(app)
-app.config['UPLOAD_FOLDER'] = os.path.realpath('.') + '/static/uploads'
-app.secret_key="slfghfghghfghghfgggj"
-ob = QUERIES()
-@app.route('/',methods=['GET','POST'])
-def home():
-    if request.method == "POST":
-        image = request.files['im']
-        if image:
-            image.save(os.path.join(app.config['UPLOAD_FOLDER'],'saheer.jpg'))
-
-
-    return render_template('layout.html')
-
-if __name__ == "__main__" :
-    app.run(debug=True)
+import pickle
+dic={"fever":"disease","alergy":"symptom","paracetamol":"medicine"}
+file_name="testfile.dat"
+fileObject=open(file_name,'wb')
+pickle.dump(dic,fileObject)
+fileObject.close()
+fileObject=open(file_name,'r')
+b=pickle.load(fileObject)
+fileObject.close()
+fileObject=open(file_name,'wb')
+dic2={"maleria":"disease"}
+dic.update(dic2)
+pickle.dump(dic,fileObject)
+fileObject.close()
+fileObject=open(file_name,'r')
+c=pickle.load(fileObject)
+print c
