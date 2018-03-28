d61af2572141019dcd3c2140a4727d034b63282c
shaheermalayil <shaheermalayil@users.noreply.github.com>
2018-03-28 11:31:18 +0530

    update

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
-
 import pickle
-dic={"fever":"disease","alergy":"symptom","paracetamol":"medicine"}
-file_name="testfile.dat"
-fileObject=open(file_name,'wb')
-pickle.dump(dic,fileObject)
-fileObject.close()
-fileObject=open(file_name,'r')
-b=pickle.load(fileObject)
-fileObject.close()
-fileObject=open(file_name,'wb')
-dic2={"maleria":"disease"}
-dic.update(dic2)
-pickle.dump(dic,fileObject)
-fileObject.close()
-fileObject=open(file_name,'r')
-c=pickle.load(fileObject)
-print c
+
+def dcitUpdate(dic,file_name="testfile.dat"):
+    fileObject = open(file_name, 'r')
+    dictionary=pickle.load(fileObject)
+    print dictionary
+dic={"sdf":"sdf"}
+dcitUpdate(dic)
+# dic={"fever":"disease","alergy":"symptom","paracetamol":"medicine"}
+# file_name="testfile.dat"
+# fileObject=open(file_name,'wb')
+# pickle.dump(dic,fileObject)
+# fileObject.close()
+# fileObject=open(file_name,'r')
+# b=pickle.load(fileObject)
+# fileObject.close()
+# fileObject=open(file_name,'wb')
+# dic2={"fever":"disease","headache":"disease"}
+# dic.update(dic2)
+# pickle.dump(dic,fileObject)
+# fileObject.close()
+# fileObject=open(file_name,'r')
+# c=pickle.load(fileObject)
+# print c
